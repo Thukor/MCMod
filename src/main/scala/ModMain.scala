eec60ae9d1ba049efaa0414398ecc688a4c01258
Christian Encarnacion <encarc@rpi.edu>
2018-06-01 16:28:20 -0400

    Base Scala Mod commit

--- src/main/scala/ModMain.scala
+++ src/main/scala/ModMain.scala
@@ -1,3 +1,23 @@
-class ModMain {
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.{FMLPostInitializationEvent, FMLPreInitializationEvent}
+
+@Mod(name = "RoadToSelyx", modid = "rts", version = ".01", acceptableRemoteVersions = "*", acceptedMinecraftVersions = "*", modLanguageAdapter = "io.github.illyohs.scorg.ScalaAdapter")
+object ModMain {
+
+  @EventHandler
+  def preInit(event: FMLPreInitializationEvent): Unit = {
+    println("Pre-Initialization!")
+  }
+
+  @EventHandler
+  def init(event: FMLPreInitializationEvent): Unit = {
+    println("Initialization!")
+  }
+
+  @EventHandler
+  def postInit(event: FMLPostInitializationEvent): Unit = {
+    println("Post-Initialization!")
+  }
 
 }
