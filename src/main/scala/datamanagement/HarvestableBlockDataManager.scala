6d5027d1cff49e07adc1dc6fcf76a8454ee4d48c
Christian Encarnacion <encarc@rpi.edu>
2018-06-03 18:14:34 -0400

    Implement first draft of harvestable block rework

---
+++ src/main/scala/datamanagement/HarvestableBlockDataManager.scala
@@ -0,0 +1,18 @@
+package datamanagement
+
+import traits.Resource
+
+import scala.collection.mutable.HashMap
+
+
+object HarvestableBlockDataManager {
+
+  private var resources = HashMap[String, Resource]()
+
+
+  def init = {
+    
+  }
+
+
+}
