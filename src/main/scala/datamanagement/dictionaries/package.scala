d46a40203bfea3fa6c79ed7b8f03620f5bdca446
Christian Encarnacion <encarc@rpi.edu>
2018-06-05 17:38:57 -0400

    Forgot to commit this..

--- src/main/scala/datamanagement/dictionaries/package.scala
+++ src/main/scala/datamanagement/dictionaries/package.scala
@@ -3,25 +3,18 @@ package datamanagement
 import datamanagement.datapacks.ResourceDataPack
 import datamanagement.jsonutil.{ItemSerializer, ResourceCategorySerializer}
 import net.liftweb.json._
-import net.liftweb.json.Serialization.{read, write}
-import util.json.JsonLoader
-import implicits.DictionaryImplicits._
-import net.liftweb.json.JsonAST.JObject
-import traits.ResourceCategory
-
-import scala.io.Source
-
+import util.json.JsonLoader._
 
 /*
   ALL DICTIONARY INSTANCES TO BE INSTANTIATED HERE
  */
 package object dictionaries {
 
+  val RESOURCEPATH = "..\\src\\main\\scala\\datamanagement\\data\\resource_blocks.json"
+
   val RESOURCEDICT: Map[String, ResourceDataPack] = {
-    val jsonString = Source.fromFile("..\\src\\main\\scala\\datamanagement\\data\\resource_blocks.json").mkString
-    implicit val formats = Serialization.formats(NoTypeHints) +
-      new ItemSerializer + new ResourceCategorySerializer
-    read[Map[String,ResourceDataPack]](jsonString)
+    implicit val resource_formats = Serialization.formats(NoTypeHints) + new ItemSerializer + new ResourceCategorySerializer
+    jsonToMap[String, ResourceDataPack](RESOURCEPATH)
   }
 
 }
