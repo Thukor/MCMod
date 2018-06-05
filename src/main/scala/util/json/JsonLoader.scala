086cf6606ea082da4b874fa7aea7985d78dfc184
Christian Encarnacion <encarc@rpi.edu>
2018-06-05 17:35:59 -0400

    Refactor jsonToMap method to take implicit parameters

--- src/main/scala/util/json/JsonLoader.scala
+++ src/main/scala/util/json/JsonLoader.scala
@@ -1,14 +1,12 @@
 package util.json
 
 import net.liftweb.json._
-
+import net.liftweb.json.Serialization.read
 import scala.io.Source
 
 object JsonLoader {
-
-  def jsonToMap[T,V](filepath: String, f: JValue => Map[T,V]): Map[T,V] = {
-    f(parse(Source.fromFile(filepath).mkString))
+  def jsonToMap[T,V](filepath: String)(implicit format: Formats, manifest: Manifest[Map[T,V]]): Map[T,V] = {
+    val jsonString = Source.fromFile(filepath).mkString
+    read[Map[T,V]](jsonString)
   }
-
-
 }
